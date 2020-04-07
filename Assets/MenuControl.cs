c5602fb92110e319c06f4bfdacc069aa94809474
rugsejo11 <augustas.cepas@gmail.com>
2020-04-07 22:18:05 +0300

    working menu
    working menu

--- Assets/MenuControl.cs
+++ Assets/MenuControl.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
+
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ButtonOptions()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void ButtonHighscores()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void ButtonDemo()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
